154a597cea1829a4e70ce0f4ee8c641635f3999a
trung nguyen thanh <trungnguyenthanh70@gmail.com>
2023-02-20 15:02:02 +0700

    update feedback 2

--- src/assets/js/components/swiper.js
+++ src/assets/js/components/swiper.js
@@ -24,6 +24,7 @@ Swiper.use([Navigation, Pagination,Autoplay ]);
         speed: 600,
         loop: false,
         slidesPerView: 1,
+       
         autoplay: {
             delay: 2500,
         },
@@ -41,6 +42,29 @@ Swiper.use([Navigation, Pagination,Autoplay ]);
             },
           },
     });
+    let swiperPerson = new Swiper(".persons-swiper", {
+        speed: 600,
+        loop: true,
+        spaceBetween: 20,
+        slidesPerView: 2,
+        autoplay: {
+            delay: 2500,
+        },
+       
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            "767": {
+              slidesPerView: 2,
+            },
+            "992": {
+              slidesPerView: 4,
+            },
+          },
+    });
+    
     let swiperContent = new Swiper(".slider-content", {
         speed: 600,
         loop: true,
